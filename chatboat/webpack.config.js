a25964678d976138f0138e31916729e32cf8d495
khiari-mohamed <mouhamed.khiari.93093292@gmail.com>
2026-02-03 12:31:39 +0100

    Restore webpack.config.js

---
+++ chatboat/webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'widget.js',
+    library: 'SuzukiChatbot',
+    libraryTarget: 'umd',
+    clean: true,
+    publicPath: '/',
+    // Ensure compatibility with WordPress
+    globalObject: 'this'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        type: 'asset/resource'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', to: '.', globOptions: { ignore: ['**/index.html'] } }
+      ]
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'REACT_APP_API_URL': JSON.stringify(process.env.REACT_APP_API_URL || 'https://suzuki-backend-lui3.onrender.com')
+      }
+    })
+  ],
+  devServer: {
+    static: [
+      { directory: path.join(__dirname, 'dist') },
+      { directory: path.join(__dirname, 'public') }
+    ],
+    port: 3000,
+    hot: true
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
